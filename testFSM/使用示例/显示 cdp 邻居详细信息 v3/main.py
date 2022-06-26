1c9be07edaaa78ea5e4fce2ad1aa01510f3fd284
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-06-26 11:26:49 +0800

    修改

---
+++ testFSM/使用示例/显示 cdp 邻居详细信息 v3/main.py
@@ -0,0 +1,26 @@
+import sys
+import textfsm
+from tabulate import tabulate
+
+template = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(template) as f, open(output_file) as output:
+    re_table = textfsm.TextFSM(f)
+    header = re_table.header
+    result = re_table.ParseText(output.read())
+    print(result)
+    print(tabulate(result, headers=header))
+
+# Filldown对 LOCAL_HOST 变量使用 action
+
+"""
+python main.py template.template command_output.txt
+[['SW1', 'SW2', '10.1.1.2', 'cisco WS-C2960-8TC-L', 'GigabitEthernet1/0/16', 'GigabitEthernet0/1', '12.2(55)SE9'], ['SW1', 'R1', '10.1.1.1', 'Cisco 3825', 'GigabitEthernet1/0/22', 'GigabitEthernet0/0', '12.4(24)T1'], ['SW1', 'R2', '10.2.2.2', 'Cisco 2911', 'GigabitEthernet1/0/21', 'GigabitEthernet0/0', '15.2(2)T1'], ['SW1', '', '', '', '', '', '']]
+LOCAL_HOST    DEST_HOST    MGMNT_IP    PLATFORM              LOCAL_PORT             REMOTE_PORT         IOS_VERSION
+------------  -----------  ----------  --------------------  ---------------------  ------------------  -------------
+SW1           SW2          10.1.1.2    cisco WS-C2960-8TC-L  GigabitEthernet1/0/16  GigabitEthernet0/1  12.2(55)SE9
+SW1           R1           10.1.1.1    Cisco 3825            GigabitEthernet1/0/22  GigabitEthernet0/0  12.4(24)T1
+SW1           R2           10.2.2.2    Cisco 2911            GigabitEthernet1/0/21  GigabitEthernet0/0  15.2(2)T1
+SW1
+"""
