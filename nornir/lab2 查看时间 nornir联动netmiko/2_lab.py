b555cae76a7f018e30791c99d4738dc1f2f9748c
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-06-29 14:44:32 +0800

    修改

--- nornir/lab2 查看时间 nornir联动netmiko/2_lab.py
+++ nornir/lab2 查看时间 nornir联动netmiko/2_lab.py
@@ -6,7 +6,7 @@ from nornir_utils.plugins.functions import print_result
 使用InitNornir()函数来初始化设备并将它赋值给变量nr。
 InitNonir()函数中需要放入config_file这个参数，针对这个参数这里我们使用的是前面提到的config.yaml这个文件， 
 """
-nr = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file=r"C:\Users\29756\PycharmProjects\network_lab\nornir\lab3\config.yaml")
 
 # 这里我们用netmiko的send_command函数向SW1和SW2执行display clock命令
 results = nr.run(netmiko_send_command, command_string='display clock utc')
