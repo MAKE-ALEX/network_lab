2a0d86bd059b6eeefe2dd44177609f5a6376631b
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-06-29 16:51:08 +0800

    修改

---
+++ nornir/lab7 用Nornir保存设备配置/7_lab.py
@@ -0,0 +1,21 @@
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.files import write_file
+from datetime import date
+
+"""
+创建一个函数backup_configuration(task)，在该函数中我们使用napalm中的get-config这个getter类API来获取设备的show run配置，
+然后调用write_file来将show_run内容写进文本文件，该文本文件名称的格式为swx-yyyy-mm-dd.txt
+"""
+
+
+def backup_configurations(task1):
+    r = task1.run(task=napalm_get, getters=["config"])
+    task1.run(task=write_file, content=r.result["config"]["running"],
+              filename=str(task1.host.name) + "-" + str(date.today()) + ".txt")
+
+
+nr = InitNornir(config_file="config.yaml")
+result = nr.run(name="正在备份交换机配置", task=backup_configurations)
+print_result(result)
