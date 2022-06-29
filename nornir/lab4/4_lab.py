cf8efb2bc463adf487f6ce38df36ce4a51bfab98
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-06-29 15:54:10 +0800

    修改

--- nornir/lab4/4_lab.py
+++ nornir/lab4/4_lab.py
@@ -3,7 +3,9 @@ from nornir_netmiko import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
-targets = nr.filter(building='1')
+# 过滤'building'为1的设备
+targets = nr.filter(level='1')
+# 查询过滤后的设备的arp表
 results = targets.run(netmiko_send_command, command_string='dis arp')
 
 print_result(results)
