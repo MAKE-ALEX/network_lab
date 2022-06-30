6a7a78655cc9e17f38b2d46aa44b244b4ecb3326
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-06-30 11:22:11 +0800

    修改

---
+++ napalm/lab1/lab1.py
@@ -0,0 +1,9 @@
+from napalm import get_network_driver
+
+driver = get_network_driver('huawei_vrp')
+SW1 = driver('192.168.59.10', 'python', '123456')
+SW1.open()
+
+output = SW1.get_arp_table()
+print(output)
+print(output[0]['mac'])
