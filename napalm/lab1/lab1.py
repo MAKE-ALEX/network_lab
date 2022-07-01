9e904629af3d5eb0c69ab4c1de9b9ac61895ec44
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-07-01 09:22:21 +0800

    修改

--- napalm/lab1/lab1.py
+++ napalm/lab1/lab1.py
@@ -1,4 +1,5 @@
 from napalm import get_network_driver
+import json
 
 driver = get_network_driver('huawei_vrp')
 SW1 = driver('192.168.59.10', 'python', '123456')
@@ -6,4 +7,4 @@ SW1.open()
 
 output = SW1.get_arp_table()
 print(output)
-
+print(json.dumps(output, indent=2))
