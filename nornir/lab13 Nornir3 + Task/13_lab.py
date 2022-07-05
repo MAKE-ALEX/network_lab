c18b4b36208f69626417154c7b77745a6d0a960e
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-07-05 17:11:48 +0800

    修改

--- nornir/lab13 Nornir3 + Task/13_lab.py
+++ nornir/lab13 Nornir3 + Task/13_lab.py
@@ -1,22 +1,29 @@
 from nornir import InitNornir
-from nornir_netmiko import netmiko_send_command
+from nornir_netmiko import netmiko_send_command, netmiko_send_config
 from nornir_utils.plugins.functions import print_result
-from nornir.core.task import Task, Result
 
-nr = InitNornir(config_file='config.yaml')
+# 使用配置文件初始化，获取回显使用模板过滤
+nr = InitNornir(config_file="config.yaml")
+output = nr.run(netmiko_send_command, command_string='display current-configuration | be interface GigabitEthernet',
+                use_textfsm=True)
 
-print(type(nr))
+# dict_keys(['sw1', 'sw2', 'sw3', 'sw4'])
+# print(output.keys())
 
+for switch in output.keys():
+    print(output[switch].result)  # 这种可以用来调测，探寻返回是啥，具体操作思路在lab11、12中介绍了。
+    for i in range(len(output[switch].result)):
+        trunk_cmd = ['interface ' + output[switch].result[i]['interface'], 'description Trunk Port (Nornir)']
+        access_cmd = ['interface ' + output[switch].result[i]['interface'], 'description Access Port to VLAN ' +
+                    (output[switch].result[i]['vlan'] if output[switch].result[i]['vlan'] else '1') + ' (Nornir)']
+        # a if a else b的逻辑，假如access接口没有配置VLAN，用VLAN 1补上
+        # print(trunk_cmd,',', access_cmd)
+        if 'trunk' in output[switch].result[i]['mode']:
+            nr.run(netmiko_send_config, config_commands=trunk_cmd)
+            print(switch, output[switch].result[i]['interface'], '已配置完成')
+        elif 'access' in output[switch].result[i]['mode']:
+            nr.run(netmiko_send_config, config_commands=access_cmd)
+            print(switch, output[switch].result[i]['interface'], '已配置完成')
 
-def host_parm(task_wgsy: Task) -> Result:
-    return (task_wgsy.host.name,
-            task_wgsy.host.hostname,
-            task_wgsy.host.username,
-            task_wgsy.host.password,
-            task_wgsy.host.platform,
-            task_wgsy.host.groups,
-            task_wgsy.host.data)
-
-
-result = nr.run(task=host_parm)
-print_result(result)
+results = nr.run(netmiko_send_command, command_string='display interface description | inc Nornir')
+print_result(results)
