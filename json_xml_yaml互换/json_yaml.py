72f7252af383591d0b157c54087a70af98d45479
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-07-05 11:27:48 +0800

    修改

--- json_xml_yaml互换/json_yaml.py
+++ json_xml_yaml互换/json_yaml.py
@@ -3,16 +3,16 @@ import json
 
 
 def yaml_transform_json(yaml_f, json_f):
-    with open('yaml_f') as f:
+    with open(yaml_f) as f:
         yaml_template = yaml.safe_load(f)
 
-    with open('json_f', 'w') as f:
+    with open(json_f, 'w') as f:
         json.dump(yaml_template, f, sort_keys=False, indent=2)
 
 
-def json_transform_yaml(json_f,yaml_f):
-    with open('json_f') as f:
+def json_transform_yaml(json_f, yaml_f):
+    with open(json_f) as f:
         json_template = json.load(f)
 
-    with open('yaml_f', 'w') as f:
+    with open(yaml_f, 'w') as f:
         yaml.dump(json_template, f, default_flow_style=False, sort_keys=False)
--- json_xml_yaml互换/main.py
+++ json_xml_yaml互换/main.py
@@ -1,4 +1,4 @@
 import json_yaml
 
-
+json_yaml.json_transform_yaml(json_f='json_text.json', yaml_f='yaml_text.yaml')
 
