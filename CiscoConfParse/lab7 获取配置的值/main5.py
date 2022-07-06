46d79952f12fcc880154afcf5cd16c26abfeb072
MAKE-ALEX <57674781+MAKE-ALEX@users.noreply.github.com>
2022-07-06 11:34:27 +0800

    修改

--- CiscoConfParse/lab7 获取配置的值/main5.py
+++ CiscoConfParse/lab7 获取配置的值/main5.py
@@ -1,19 +1,21 @@
-from pprint import pprint
 from ciscoconfparse import CiscoConfParse
 
-# 从cisco1.txt中获取SVI10下的HSRP地址
 parse = CiscoConfParse('./cisco1.txt')
 
 intf_obj = parse.find_objects(r'^interface\s+Vlan10$')[0]
 
-hsrp_ip = intf_obj.re_match_iter_typed(r'standby\s10\sip\s(\S+)',default='')
+arp_timeout = intf_obj.re_match_iter_typed(r'arp\s+timeout\s+(\d+)', result_type=int, default=4 * 3600)
 
-print(hsrp_ip)
+print(arp_timeout)
 
 """
+使用re_match_iter_typed()，转换数据类型。把SVI10下的arp超时时间从str转换成int
+
 re_match_iters_typed()有2个关键字属性：default、untyped_default
 
 （1）default 关键字的意思是，如果正则表达式与配置不匹配时的默认值，该默认值会自动转换为result_type。
 
 （2）untyped_default，如果不希望默认值转换为result_type，可以使用untyped_default=True
 """
+
+
--- CiscoConfParse/lab7 获取配置的值/main6.py
+++ CiscoConfParse/lab7 获取配置的值/main6.py
@@ -1,16 +1,17 @@
-from pprint import pprint
 from ciscoconfparse import CiscoConfParse
 
-# 从cisco1.txt中获取SVI10下的HSRP地址
 parse = CiscoConfParse('./cisco1.txt')
 
-intf_obj = parse.find_objects(r'^interface\s+Vlan10$')[0]
+intf_obj = parse.find_objects(r'^interface\s+Vlan20$')[0]
 
-hsrp_ip = intf_obj.re_match_iter_typed(r'standby\s10\sip\s(\S+)',default='')
+arp_timeout = intf_obj.re_match_iter_typed(r'arp\s+timeout\s+(\d+)', result_type=int, untyped_default=True,
+                                           default=f'__no_explicit_value__')
 
-print(hsrp_ip)
+print(arp_timeout)
 
 """
+实验untype_default属性
+
 re_match_iters_typed()有2个关键字属性：default、untyped_default
 
 （1）default 关键字的意思是，如果正则表达式与配置不匹配时的默认值，该默认值会自动转换为result_type。
